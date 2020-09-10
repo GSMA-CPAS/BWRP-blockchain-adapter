7367b9c7b9770cb680356eef6ae081389b7025ae
Simon Schulz <simon.schulz01@telekom.de>
2020-09-10 16:10:38 +0200

    cleanup and prepare for client api

--- server/config.js
+++ server/config.js
@@ -10,6 +10,6 @@ const config = {
 };
 //config.OPENAPI_YAML = path.join(config.ROOT_DIR, 'api', 'openapi.yaml');
 //config.FULL_PATH = `${config.URL_PATH}:${config.URL_PORT}/${config.BASE_VERSION}`;
-config.FILE_UPLOAD_PATH = path.join(config.PROJECT_DIR, 'uploaded_files');
+//config.FILE_UPLOAD_PATH = path.join(config.PROJECT_DIR, 'uploaded_files');
 
 module.exports = config;
