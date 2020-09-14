d649adff07fde73752339c4645943295c1f593df
Simon Schulz <simon.schulz01@telekom.de>
2020-09-14 14:44:51 +0200

    implement verification

--- client/index.js
+++ client/index.js
@@ -1,18 +1,28 @@
 console.log("starting example-client")
 var api = require('blockchain-adapter-api')
-var btoa = require('btoa');
+var btoa = require('btoa')
 var crypto = require('crypto');
 const { exit } = require('process');
-const openssl = require('openssl-nodejs')
 const _ = require('lodash');
+const fs = require('fs');
+
+const openssl = require('openssl-nodejs')
+//the strange openssl lib needs this dir in order to work
+fs.mkdir("openssl", { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
 
 const ENDPOINT_DTAG = "http://localhost:8081"
 const ENDPOINT_TMUS = "http://localhost:8082"
 
-//DOCUMENT="MYDOCUMENT_DATA_123456..."
 // create a unique document:
-const DOCUMENT = new Date().getTime() //"test"
+// WARNING: btoa() does not seem to work reliable on real binary documents!
+const DOCUMENT = (new Date().getTime()).toString() //"test"
 const DOCUMENT64 = btoa(DOCUMENT)
+
+console.log(DOCUMENT)
+console.log(DOCUMENT64)
 const DOCUMENTSHA256 = crypto.createHash('sha256').update(DOCUMENT64).digest('hex');
 console.log("DOCUMENT: " + DOCUMENT+", DOCUMENT64: " + DOCUMENT64+", DOCUMENTSHA256: " + DOCUMENTSHA256)
 
@@ -30,26 +40,72 @@ function setOffchainDBAdapterConfig (config, name, uri) {
 }
 
 function signContract(signer, document) {
+    
     return new Promise((resolve, reject) => {
         var document_sig = new api.DocumentSignature()
         document_sig.signer = signer
         
         // create key
-        openssl("openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -nodes -keyout key -out crt -subj /CN="+signer+"/C=DE/ST=NRW/L=Bielefeld/O=ORG/OU=ORGOU -addext keyUsage=digitalSignature", function(err, b){
-            //console.log(err.toString())
-        }).on('close', (code) => { if (code != 0) throw("openssl failed")})
-    
+        openssl("openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -nodes -keyout key_"+signer+" -out crt -subj /CN="+signer+"/C=DE/ST=NRW/L=Bielefeld/O=ORG/OU=ORGOU -addext keyUsage=digitalSignature", function(e, b){
+            //
+        }).on('close', (code) => { 
+            if (code != 0) reject("openssl: failed to create key")
+        })
+
         // extract pem
         var pembuf = Buffer.alloc(0)
         openssl(['x509', '-pubkey', '-in', 'crt'], function(e,b){
+            if ((e) && (e != "")) reject(e.toString())
+
             document_sig.pem = b.toString()
             var signature = ""
-            var docbuf = Buffer.from(document)
-            openssl(['dgst', '-sha256', '-sign', 'openssl/key', { name: "doc", buffer: docbuf}], function(e,b){
-                document_sig.signature = btoa(b)
-                resolve(document_sig)
-            }).on('close', (code) => { if (code != 0) throw("openssl failed")})
-        }).on('close', (code) => { if (code != 0) throw("openssl failed")});
+            var docBuf = Buffer.from(document)
+            var sigBuf = Buffer.alloc(0)
+            console.log("> DOC <"+document+">")
+            openssl(['dgst', '-sha256', '-sign', 'openssl/key_'+signer, '-out',  { name: "signature_"+signer, buffer: sigBuf}, { name: "doc", buffer: docBuf}], function(e,sig_bin){
+                if ((e) && (e != "")) reject(e.toString())
+                // do base64 encoding
+                openssl(['enc', '-A', '-base64', '-in', 'signature_' + signer], function(e,b){
+                    if ((e) && (e != "")) reject(e.toString())
+                    document_sig.signature = b.toString()
+                    resolve(document_sig)
+                }).on('close', (code) => { 
+                    if (code != 0) reject("openssl: failed to calc base64")
+                })
+            }).on('close', (code) => { 
+                if (code != 0) reject("openssl: failed to sign")
+            })
+        }).on('close', (code) => { 
+            if (code != 0) reject("openssl: failed to extract pem")
+        });
+    })
+}
+
+
+function verifySignature(document, pem, signature64) {
+    console.log("document: " + document+".")
+    console.log("pem: " + pem)
+    console.log("signature64: " + signature64)
+    
+    return new Promise((resolve, reject) => {
+        var docBuf = Buffer.from(document)
+        var pemBuf = Buffer.from(pem)
+        var sigBuf = Buffer.from(signature64)
+
+
+        openssl(['enc', '-d', '-A', '-base64', '-in', { name: 'signature_sig64', buffer: sigBuf}, '-out', 'signature_sig'], function(e,b){
+            if ((e) && (e != "")) reject(e.toString())
+
+            openssl(['dgst', '-sha256', '-verify', { name: "signature_pem", buffer: pemBuf }, '-signature', 'openssl/signature_sig', { name: "signature_doc", buffer: docBuf }], function(e,b){
+                if ((e) && (e != "")) reject(e.toString())
+
+                resolve( b.toString() )
+            }).on('close', (code) => {
+                 if (code != 0) reject("openssl: failed to verify signature")
+            })
+        }).on('close', (code) => { 
+            if (code != 0) reject("openssl: failed to calc base64")
+        })
     })
 }
 
@@ -98,16 +154,23 @@ const launchClient = async () => {
         var res = await DTAG.contract.fetchSignatures(DOCUMENTSHA256, "TMUS")
         var sig = res.body
 
-        _.each(sig, function (value, key) {
-            console.log("> verifying signature[" + key + "] = " + value.signature)    
-            //TODO openssl("openssl dgst -sha256 -verify openssl$PUB_TMUS -signature /dev/stdin $DOC
-            console.log("TODO!")
+        _.each(sig, function (entry, key) {
+            console.log("> verifying signature[" + key + "] = " + entry.signature)    
+            verifySignature(doc.data, entry.pem, entry.signature).then ( result => {
+                console.log("> result: " + result)
+            }).catch( (x) => {
+                console.log(" FAILED: " + x)
+            })
         });
         
     } catch (err) {
-        console.error(err)
+        if ((err.body) && (err.body.message)) {
+            console.log(err.body.message)
+        } else {
+            console.error("ERROR: >" + err + "<")
+        }
     }
 
 }
 
-launchClient().catch(e => logger.error(e));
+launchClient().catch(e => console.log(e));
