67bc37f0590c5cd1a0cb23301b8488eb1ab85cf0
Simon Schulz <simon.schulz01@telekom.de>
2020-11-05 08:34:06 +0100

    add couchdb to tunnel

--- server/tools/tunnel.sh
+++ server/tools/tunnel.sh
@@ -4,4 +4,4 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-ssh -L 7051:localhost:7051 -L 9051:localhost:9051 -L 10051:localhost:10051 -L 7050:localhost:7050 -L 8080:localhost:8080 -L 8081:localhost:8081  -L 8082:localhost:8082 $1
+ssh -L 7051:localhost:7051 -L 9051:localhost:9051 -L 10051:localhost:10051 -L 7050:localhost:7050 -L 8080:localhost:8080 -L 8081:localhost:8081  -L 8082:localhost:8082 -L 5984:localhost:5984 $1
