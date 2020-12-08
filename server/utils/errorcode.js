4f2eb196b38333aff9832af5c388320da26486ae
Simon Schulz <65604266+sschulz-t@users.noreply.github.com>
2020-12-08 09:10:05 +0100

    rework error forwarding to detect and handle fabric errors properly (#12)

--- server/utils/errorcode.js
+++ server/utils/errorcode.js
@@ -1,3 +1,4 @@
+
 /** ErrorCode implements a custom error class */
 class ErrorCode extends Error {
   /** process a chaincode error, parse the response and return a proper error
@@ -7,37 +8,49 @@ class ErrorCode extends Error {
   constructor(code, message) {
     super(message);
     this.code = code;
+    // add this for nicer toString() prints:
+    this.name = code;
   }
 
-
-  /** process a chaincode error, parse the response and return a proper error
-  * @param {string} errorJSON - the chaincode error (json format)
+  /** process any error, parse the response and return a proper error type
+  * @param {string} error - the error
   * @param {string} userMessage - the error message that should be forwareded to the user
   * @return {ErrorCode} a new ErrorCode
   */
-  static fromChaincodeError(errorJSON, userMessage) {
-    // try to decode the json error message
+  static fromError(error, userMessage) {
+    // try to decode the error message
     // note: ideally i would assume go chaincode and node sdk can deliver the proper error message
     //       however, i did not find an easy way to pass a proper error from go to node?!
-    const msg = errorJSON.message;
-    let code = 'UNDEFINED';
 
-    try {
-      const reply = JSON.parse(msg);
-      // msg was parsed, craft custom error:
-      code = reply.code;
-      // always show full error on log:
-      console.log('ERROR: ' +code + ' ' + userMessage + ', origin error was: ' + reply.message);
-    } catch (error) {
-    // failed to parse, abort here
-      code = 'ERROR_BAD_JSON';
-      userMessage = 'failed to parse json: can not parse error return value. see blockchain adapter log!';
-      console.log('ERROR: '+code+' failed to parse error payload -'+msg+'- as json:');
+    // decide if it this is a chaincode/fabric error:
+    if (error.message.charAt(0) == '{') {
+      // this looks like out custom chaincode error, lets try to decode it:
+
+      let code = 'UNDEFINED';
+
+      try {
+        const reply = JSON.parse(error.message);
+        // msg was parsed, craft custom error:
+        code = reply.code;
+        // always show full error on log:
+        console.log('ERROR: ' +code + ' ' + userMessage + ', origin error was: ' + reply.message);
+      } catch (error) {
+        // failed to parse, abort here
+        code = 'ERROR_BAD_JSON';
+        userMessage = 'failed to parse json: can not parse error return value. see blockchain adapter log!';
+        console.log('ERROR: '+code+' failed to parse error payload -'+error.message+'- as json:');
+        console.log(error);
+      }
+
+      // forward to application for further processing
+      return new ErrorCode(code, userMessage);
+    } else {
+      // fabric error
+      const code = 'ERROR_INTERNAL';
+      console.log('ERROR: '+code+', '+userMessage+': fabric produced the following error: ');
       console.log(error);
+      return new ErrorCode(code, userMessage);
     }
-
-    // forward to application for further processing
-    return new ErrorCode(code, userMessage);
   }
 }
 
